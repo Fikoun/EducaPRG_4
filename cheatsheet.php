361495d35af2a6e15aec453851d8544a8bc7bd1e
Filip Janko <fikoun@icloud.com>
2025-10-22 12:18:32 +0200

    Add examples of loops and arrays in cheatsheet.php

--- cheatsheet.php
+++ cheatsheet.php
@@ -126,3 +126,112 @@ if ($vek < 15) {
     }
 
 }
+
+
+////////////////////////////////////////////
+///     CYKLY - looping / opakování      ///
+////////////////////////////////////////////
+// Cyklus je jako podmínka jenom se jeji obsah může vykonat víckrát
+// Základní cyklus je while()
+while ($dokudPravda) {
+    // ZDE Kód,
+    // Co
+    // Se
+    // Opakuje! dokud platí podmínka
+}
+// Např.:
+$pocitadlo = 10;
+while ($pocitadlo != 0) {
+    echo $pocitadlo; // Vypíše každé druhé číslo od 10
+    $pocitadlo -= 2; // Kdybychom hodnotu nezměnily nikdy by cyklus nepřestal
+}
+echo "Vypíše se pouze jendou"; // Cyklus skončí } a pokračuje
+
+///////////////////
+///  for cykly  ///
+///////////////////
+// Pro lepší ovládání používáme "for loops"
+for ($pocitadlo = 10; $pocitadlo != 0; $pocitadlo--) {
+    echo $pocitadlo . "<br>"; // Vypíše každé číslo na řádek
+}
+
+#
+##
+###
+####
+// Příklad: Cyklus v Cyklu (2-render stromeček)
+$znakPixelu = '#';
+$vyska = 5;
+for ($radky = 0; $radky < $vyska; $radky++) {
+    // Bude víc a víc znaku na řáešk podle toho kolikátý řádek toe
+    $kolikZnakuNaRadek = $radky + 1;
+
+    for ($linka = 0; $linka < $kolikZnakuNaRadek; $linka++) {
+        // Na kazdem probehne cyklu pro vypsání znaků za sebou
+        echo $znakPixelu;
+    }
+
+    // Po dokončení znaků na řádku zakončíme a jdeme na další
+    echo '<br>';
+}
+// !nad tímto příkladem je ukázka stromečku/trojúhelníku ze znaků
+
+
+
+//////////////////////////////////////
+///     POLE - list / seznam      ///
+/////////////////////////////////////
+/// DŮLEŽITÝ Koncept! - vše je založené na polých (anglicky ARRAY)
+// Můžeme sepsat uživatele, produkty, itemy nebo čísla ke spočítání výdělků
+$cisla = [3, 7, 9, 1, 10];
+$jmena = ["Pepa", "Anna", "Greq"];
+
+// Vloží nový prvek na konec!
+$jmena[] = "Honza"; // ["Pepa", "Anna", "Greq", "Honza"]
+
+// Ke konkrétnímu prvku pole přistoupíme takto
+$jmena[0]; // nultý index je první člen/prvek
+// Můžeme..
+$jmena[0] = "Pepina"; // změní prvek 
+$jmena[0] *= "Pepina"; // vynásobit (vydelit, secist, odecist, atd..)
+
+// Pozor na rozdíl !!!
+print_r($jmena[0]); // vypíše prvního => "Pepa"
+print_r($jmena); // nebo vypíšeme celé pole ["Pepa", "Anna", "Greq"]
+
+
+///     JAK RUČNĚ VYPSAT POLE      ///
+$pocet = count($jmena);
+for ($index = 0; $index < $pocet; $index++) {
+    echo "Jmeno: " . $jmena[$index] . "je na indexu" . $index;
+    echo "<br>";
+}
+
+
+///     JAK SEČÍST POLE      ///
+$pocetCisel = count($cisla);
+$celkem = 0; // zacneme na nule a budeme přičítat
+for ($i = 0; $i < $pocetCisel; $i++) {
+    $prvek = $cisla[$i];
+    $celkem += $prvek;
+}
+echo "Součet: " . $celkem;
+echo "Průměr: " . ($celkem / $pocetCisel);
+
+
+/////////////
+// FOREACH //
+/////////////
+// Namísto klasického for() použijeme foreach($array as $prvek)
+// Jednoduší stavba, ale menší kontrola na cyklem
+foreach ($jmena as $konkretniJmeno) {
+    echo $jmeno . ', '; // Vypíšeme včechny prvky
+}
+
+
+///     JAK SEČÍST POLE      ///
+// Oproti kódu for() je jednodužší (viz. víše)
+$celkem = 0;
+foreach ($jmena as $prvek) {
+    $celkem += $prvek;
+}
